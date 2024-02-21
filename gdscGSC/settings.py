c12354965b4e940edbf2753f9ce0a349123f2b9e
Abdullahzg <50936044+Abdullahzg@users.noreply.github.com>
2024-02-22 02:14:27 +0500

    vv

--- gdscGSC/settings.py
+++ gdscGSC/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3nyrgy=x$2hk1s501b#wz3j9&!l6cy@+-c3ivwui3k-4alt!_9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 
