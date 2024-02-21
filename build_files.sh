ceb1baf49fca3c7d9a43eb8b4626f3b315e3ec3c
Abdullahzg <50936044+Abdullahzg@users.noreply.github.com>
2024-02-22 01:20:36 +0500

    dd

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,7 @@
 echo " BUILD START"
 
 python3.9 -m pip install -r requirenments.txt
-python3.9 -m pip install Django
+python3.9 manage.py collectstatic
 python3.9 manage.py makemigrations
 python3.9 manage.py migrate
 
--- gdscGSC/urls.py
+++ gdscGSC/urls.py
@@ -16,10 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from django.conf import settings
 from mainweb.views import home_screen_view
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_screen_view, name="home"),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
