65bb5b0d4a7d52bb0d361f05afb5922d30f9f582
Abdullahzg <50936044+Abdullahzg@users.noreply.github.com>
2024-02-22 01:47:28 +0500

    fcf

--- gdscGSC/wsgi.py
+++ gdscGSC/wsgi.py
@@ -8,10 +8,12 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gdscGSC.settings')
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
 application = get_wsgi_application()
 
