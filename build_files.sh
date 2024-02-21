5ce8fce7c2a9a0bddb32c941c64729022f0d53b7
Abdullahzg <50936044+Abdullahzg@users.noreply.github.com>
2024-02-22 01:03:05 +0500

    a

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,7 @@
 echo " BUILD START"
-python3.9 -m pip freeze | xargs pip uninstall -y
+
 python3.9 -m pip install -r requirenments.txt
+python3.9 -m pip install Django
 python3.9 manage.py makemigrations
 python3.9 manage.py migrate
 
