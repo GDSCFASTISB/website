c7b9e14dd11df15a72fba3f7f1fb3baf5e9411ec
Abdullahzg <50936044+Abdullahzg@users.noreply.github.com>
2024-02-21 23:32:24 +0500

    bruh

---
+++ README.md
@@ -0,0 +1,2 @@
+# SolutionChallenge-Website-
+This repository is for the Website giving the introduction and insight of our project
---
+++ build_files.sh
@@ -0,0 +1,7 @@
+echo " BUILD START"
+cd /src
+python3.9 -m pip install -r requirements.txt
+python3.9 manage.py collectstatic
+python3.9 manage.py makemigrations
+python3.9 manage.py migrate
+echo " BUILD END" 
