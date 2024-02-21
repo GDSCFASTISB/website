9c1948be3f2232606095e32b3366cdf62c2d0bae
Abdullahzg <50936044+Abdullahzg@users.noreply.github.com>
2024-02-22 00:29:43 +0500

    yar

--- build_files.sh
+++ build_files.sh
@@ -2,5 +2,5 @@ echo " BUILD START"
 python3.9 -m pip install -r requirenments.txt
 python3.9 manage.py makemigrations
 python3.9 manage.py migrate
-python3.9 manage.py runserver
+
 echo " BUILD END" 
